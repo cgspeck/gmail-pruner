c8b1f1ab086ef66217d3be14a96ae3863c643e1e
Chris Speck <cgspeck@gmail.com>
2017-10-02 22:22:40 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Gmail Pruner
+
+Quick and dirty way to keep your mail box in check. 
+
+Inspired by this [post](http://radtek.ca/blog/delete-old-email-messages-programatically-using-python-imaplib/).
+
+***USE AT YOUR OWN RISK!***
+
+**This is explicitly designed to facilitate the total and irrecoverable deletion of your email. It is licensed to you under the MIT License underwhich you agree that this software is provided "as is"; without warranty of any kind; and to not hold the authors liable for any claim, damages or other liability. You may read the [full text of the license here.](./LICENSE.md)**
+
+## Requirements
+
+Enable IMAP access on your gmail account. Create an application password (if using two-factor) or find the option to "Enable Less Secure Apps".
+
+## Running
+
+```
+$ python prune.py 
+usage: prune.py [-h] folder age
+prune.py: error: the following arguments are required: folder, age
+
+$ GMAIL_PASS={password} GMAIL_USER='{username}@gmail.com' python prune.py '[Gmail]/Sent Mail' 365
+```
+
+## Installation with systemd
+
+1. Copy `prune.py` to a location.
+
+2. Make copies of `prune-sent-mail.service.example` and `prune-sent-mail.timer.example`
+    a) change the name of the service/timer, file names, set age and folder
+    b) remove `.example` from file names
+
+3. Run `sudo systemctl daemon-reload`.
+
+4. Run `sudo systemctl edit {name}` and set `GMAIL_USER` and `GMAIL_PASS` following this [ServerFault Answer](https://serverfault.com/a/413408/92665).
+
+5. Start and Enable the timer with `systemctl start {name}.timer` and `sudo systemctl enable {name}.timer`.
---
+++ app/prune.py
@@ -0,0 +1,86 @@
+import argparse
+import datetime
+import imaplib
+import os
+import sys
+
+def connect_imap(username, password):
+    m = imaplib.IMAP4_SSL("imap.gmail.com")  # server to connect to
+    print("{0} Connecting to mailbox via IMAP...".format(datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+    m.login(username, password)
+    return m
+
+
+def delete_old_mail(m, folder, days_before):
+    no_of_msgs = int(m.select(f'"{folder}"')[1][0])
+    print("- Found a total of {1} messages in '{0}'.".format(folder, no_of_msgs))
+
+    if not m.state == 'SELECTED':
+        return
+
+    before_date = (datetime.date.today() - datetime.timedelta(days_before)).strftime("%d-%b-%Y")  # date string, 04-Jan-2013
+    typ, data = m.search(None, '(BEFORE {0})'.format(before_date))  # search pointer for msgs before before_date
+
+    if not data[0] == b'':  # if not empty list means messages exist
+        no_msgs_del = data[0].split()[-1].decode()  # last msg id in the list
+        print("- Marked {0} messages for removal with dates before {1} in '{2}'.".format(no_msgs_del, before_date, folder))
+        m.store(f"1:{no_msgs_del}", '+FLAGS', '\\Deleted')  # delete immediately
+    else:
+        print("- Nothing to remove.")
+
+
+def empty_folder(m, folder, do_expunge=True):
+    print("- Empty '{0}' & Expunge all mail...".format(folder))
+    m.select(folder)  # select all trash
+    if m.state == 'SELECTED':
+        m.store("1:*", '+FLAGS', '\\Deleted')  # Flag all Trash as Deleted
+        if do_expunge:  # See Gmail Settings -> Forwarding and POP/IMAP -> Auto-Expunge
+            m.expunge()  # not need if auto-expunge enabled
+        else:
+            print("Expunge was skipped.")
+    else:
+        print("Nothing selected for deletion")
+
+    return
+ 
+ 
+def disconnect_imap(m):
+    print("{0} Done. Closing connection & logging out.".format(datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+    m.close()
+    m.logout()
+    return
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Prune a Gmail folder via IMAP.')
+    parser.add_argument(
+        'folder',
+        nargs=1,
+        help='Folder to remove, e.g. \'[Gmail]/Sent Mail\''
+    )
+
+    parser.add_argument(
+        'age',
+        nargs=1,
+        type=int,
+        help='Delete email over this many days old'
+    )
+    args = parser.parse_args()
+    age = args.age[0]
+    folder = args.folder[0]
+
+    if age < 7:
+        print("Age must be greater than 7 days", file=sys.stderr)
+        sys.exit(1)
+
+    username = os.environ.get('GMAIL_USER')
+    password = os.environ.get('GMAIL_PASS')
+
+    if not (username and password):
+        print("Please set GMAIL_USER and GMAIL_PASS environment variables!", file=sys.stderr)
+        sys.exit(1)
+
+    m_con = connect_imap(username, password)
+    delete_old_mail(m_con, folder, age)
+    empty_folder(m_con, '[Gmail]/Bin', do_expunge=False)
+    disconnect_imap(m_con)
+
